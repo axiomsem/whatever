85df75ae67b3070a685ca9995f29e744d442b96c
Jah Rule <=>
2022-11-03 20:24:38 -0700

    weak key up down infra

--- whatever/SceneView.h
+++ whatever/SceneView.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SceneView : MTKView
 
+- (bool)hasCharKeyDown:(char)character;
+
 - (void)keyDown:(NSEvent *)theEvent;
 -(id)initWithFrame:(NSRect)frame;
 - (BOOL)acceptsFirstResponder;
--- whatever/SceneView.m
+++ whatever/SceneView.m
@@ -7,7 +7,12 @@
 
 #import "SceneView.h"
 
+typedef bool _KeyBuffer[256];
+
 @implementation SceneView
+{
+    _KeyBuffer _keys;
+}
 
 - (void)drawRect:(NSRect)dirtyRect {
     [super drawRect:dirtyRect];
@@ -21,15 +26,36 @@
 {
     [super keyDown:theEvent];
     
-    NSLog(@"onKeyDown Detected; Merry Christmas, by the way.");
+    for (size_t i = 0; i < theEvent.characters.length; ++i) {
+        _keys[i] = true;
+    }
+    
+    NSLog(@"onKeyDown Detected; %@", theEvent.characters);
+}
+
+- (void)keyUp:(NSEvent *)theEvent
+{
+    [super keyUp:theEvent];
+    
+    for (size_t i = 0; i < theEvent.characters.length; ++i) {
+        _keys[i] = false;
+    }
+    
+    NSLog(@"onKeyUp Detected; %@", theEvent.characters);
 }
 
 -(id)initWithFrame:(NSRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
+        memset(_keys, 0, sizeof(_keys));
     }
     return self;
+}
+
+- (bool)hasCharKeyDown:(char)character
+{
+    return _keys[(size_t)character];
 }
 
 - (BOOL)becomeFirstResponder
